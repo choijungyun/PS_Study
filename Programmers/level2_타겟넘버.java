e46703099601dc33c301c675316fd941e3c2acad
Jungyun <cjy8529@naver.com>
2021-01-19 16:41:40 +0900

    프로그래머스 level2_타겟넘버

---
+++ Programmers/level2_타겟넘버.java
@@ -0,0 +1,23 @@
+package Programmers;
+import java.util.*;
+import java.io.*;
+
+public class level2_Å¸°Ù³Ñ¹ö {
+	 public int answer=0;
+	    public int solution(int[] numbers, int target) {
+	        dfs(0, 0, target, numbers);
+	        return answer;
+	    }
+	    
+	    public void dfs(int idx, int sum, int target, int[] numbers){
+	        if(idx==numbers.length){
+	            if(sum==target){
+	                answer++;
+	            }
+	        }
+	        else{
+	            dfs(idx+1, sum+numbers[idx], target, numbers); 
+	            dfs(idx+1, sum-numbers[idx], target, numbers);        
+	        }
+	    }
+}
